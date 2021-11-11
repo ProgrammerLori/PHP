5ac9a9e53b9e50d240b21543488c79615b68908b
ProgrammerLori <92303806+ProgrammerLori@users.noreply.github.com>
2021-11-11 11:57:00 +0100

    remove_id

--- model/Hianyzo.php
+++ model/Hianyzo.php
@@ -7,6 +7,14 @@ class Hianyzo extends Kijeloltfelhasznalok {
     function __construct() {
         $this->tablaNev = 'hianyzok';
     }
+
+function remove_id($id,$conn)
+{
+    $sql = "DELETE FROM hianyzok WHERE id =".$_GET['nem_hianyzo'];
+	$result = $conn->query($sql);	
+}
+
+
 }
 
 ?>
